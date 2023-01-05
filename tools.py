c5c86008981a2a50de45ba6dca2da13d88a90456
Evert <evertdevroey@gmail.com>
2023-01-05 22:46:55 +0100

    initial commit

---
+++ tools.py
@@ -0,0 +1,23 @@
+def set_output_file():
+    output_file = input("Destination file (.txt): ")
+
+    if output_file[-4:] != '.txt':
+        output_file += '.txt'
+
+    print(f"\n * Output file changed to {output_file} *")
+
+    return output_file
+
+
+def set_words_pars():
+    while True:
+        try:
+            words = int(input("Number of words: "))
+            break
+        except ValueError:
+            print("Enter a valid integer")
+
+    pars = int(words // 100) + 2
+    words *= 1.12
+
+    return words, pars
